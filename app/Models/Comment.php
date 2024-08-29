8b683d24a77dcb87b2889c3620506f2e11054cb2
Ali Ilyes MANSOUR <mansour.aliilyes@gmail.com>
2024-08-29 15:34:32 +0100

    Relationships between models

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    public function commentable()
+    {
+        return $this->morphTo();
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Comment::class, 'parent_id');
+    }
 }
